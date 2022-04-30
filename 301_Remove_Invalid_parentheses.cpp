fbc69a22d3c9aed96e0743e9835d7357f8b972c9
Brajmohan Kumar Singh <62195966+Brajmohank1@users.noreply.github.com>
2022-04-30 12:53:32 +0530

    Update 301_Remove_Invalid_parentheses.cpp

--- 301_Remove_Invalid_parentheses.cpp
+++ 301_Remove_Invalid_parentheses.cpp
@@ -1,3 +1,23 @@
+/*
+Given a string s that contains parentheses and letters, remove the minimum number of invalid parentheses to make the input string valid.
+
+Return all the possible results. You may return the answer in any order.
+
+ 
+
+Example 1:
+
+Input: s = "()())()"
+Output: ["(())()","()()()"]
+Example 2:
+
+Input: s = "(a)())()"
+Output: ["(a())()","(a)()()"]
+
+
+*/
+
+
 class Solution {
 public:
     
